d0f906455e26a80b79290b375fc3319744e9eeaf
ElleeB <laura.trager@gmail.com>
2018-11-03 13:19:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// returns a function that we can then to 
+// returns a function that we can then to
 // calculate if a trip is too large for a driver
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
@@ -8,6 +8,26 @@ function produceDrivingRange(blockRange) {
       return `within range by ${blockRange - rangeResult}`
     }else {
       return `${rangeResult - blockRange} blocks out of range`
+    }
+  }
+}
+
+// Returns a function that then calculates a tip
+function produceTipCalculator(tipPercentage) {
+  return function(fare) {
+    return fare * tipPercentage
+  }
+}
+
+// a function that returns a Driver class
+
+function createDriver() {
+  let driverId = 0
+
+  return class {
+    constructor(name) {
+      this.name = name
+      this.id = driverId++
     }
   }
 }
